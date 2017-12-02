e887fa42ec6b367719360ec0f8a318a6e6bee47c
Jiří Beneš <mail@jiribenes.com>
2017-12-03 00:27:05 +0100

    Day 2 - hard

--- src/day2.rs
+++ src/day2.rs
@@ -1,6 +1,6 @@
 use std::cmp;
 
-fn solve_row(row: &[u32]) -> u32 {
+fn solve_row_easy(row: &[u32]) -> u32 {
     let first: u32 = *row.first().unwrap_or(&0);
 
     let (min, max) = row.iter().fold((first, first), |(min, max), &x| {
@@ -8,6 +8,18 @@ fn solve_row(row: &[u32]) -> u32 {
     });
 
     max - min
+}
+
+fn solve_row_hard(row: &[u32]) -> u32 {
+    for &x in row {
+        for &y in row {
+            if x != y && x % y == 0 {
+                return x / y;
+            }
+        }
+    }
+
+    0
 }
 
 fn parse_row(line: &str) -> Vec<u32> {
@@ -21,8 +33,20 @@ pub fn solve() {
     let easy: u32 = input
         .lines()
         .map(|line| parse_row(line))
-        .map(|row| solve_row(&row))
+        .map(|row| solve_row_easy(&row))
         .sum();
 
-    println!("{:?}", easy);
+    let hard: u32 = input
+        .lines()
+        .map(|line| parse_row(line))
+        .map(|row| solve_row_hard(&row))
+        .sum();
+
+    println!("easy: {}, hard: {}", easy, hard);
+}
+
+#[test]
+fn test_solve_hard() {
+    assert_eq!(solve_row_hard(&vec![1, 2, 3, 4]), 2);
+    assert_eq!(solve_row_hard(&vec![10, 12, 5, 7, 9]), 2);
 }
